e4437a50edd74ea8fc5854b481b4b6506fd7b2fa
adhyayan-asia <47819124+adhyayan-asia@users.noreply.github.com>
2019-02-22 15:02:36 +0530

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -10,25 +10,18 @@ if(DEVELOPMENT_ENVIRONMENT){
 	define("SITEURL","https://localhost/XYZ/"); // Please set your website url eg: https://localhost/XYZ/
 }
 
-define("TOKEN_LIFE",12000); //in Seconds
-define("INDIAN_SCHOOL_BORAD",101); 
-define("DEFAULT_COUNTRY_ID",101); 
-define("DEFAULT_DIAGNOSTIC",2); 
-define("DEFAULT_TIER",3); 
-define("DEFAULT_AWARD_STANDARD",1);
-define("DEFAULT_AQS_ROUND",1);
-define("RATING_OTHERS_ID",39);
-define('KEY_BEHAVIOURS','Objectivity,Positivity,Maintaining confidentiality');
-define('CLASSROOM_OBSERVATION','Quietly find a seat or space at the back of the room to observe~Spend between 5 to 7 minutes in a class');
-define('STAKEHOLEDER','Introduce yourself, the purpose of the review and share that all responses will be confidential~Ask for examples and documentation to verify the information~Adapt language to suit the stakeholder being interviewed');
-define('SCHOOL_RATING','The meetings are irregular~There is some contribution to events from the teachers, and there is some documented evidence of opportunities to meet.~The meeting minutes indicate that meetings are directed by the Principal');
-
-date_default_timezone_set("Asia/Kolkata");
-define("DEFAULT_STUDENT_PROFILE_ATTRIBUTE",49);
-define('OFFLINE_STATUS', FALSE); 
-define("FEEDBACK_ROLES",'8,1,2');
-define("DIAGNOSTIC_LANG",'hi,en,ma');
-define("DEFAULT_LANGUAGE",'9');
-define("Vikas",1);
-define("DOWNLOAD_CHART_URL","http://13.127.82.199:7801/");
+define("TOKEN_LIFE",12000); //Users session expires time(in Seconds)
+define("DEFAULT_TIER",3); // Default award is state award(3=state)
+define("DEFAULT_AWARD_STANDARD",1); // Default standard award is international award(1=international)
+define("DEFAULT_AQS_ROUND",1); // Default aqs round is 1
+define("RATING_OTHERS_ID",39); // Match rating-id to validate
+define('KEY_BEHAVIOURS','Objectivity,Positivity,Maintaining confidentiality'); // KEY_BEHAVIOURS text
+define('CLASSROOM_OBSERVATION','Quietly find a seat or space at the back of the room to observe~Spend between 5 to 7 minutes in a class'); //CLASSROOM_OBSERVATION text
+define('STAKEHOLEDER','Introduce yourself, the purpose of the review and share that all responses will be confidential~Ask for examples and documentation to verify the information~Adapt language to suit the stakeholder being interviewed'); //STAKEHOLEDER text
+define('SCHOOL_RATING','The meetings are irregular~There is some contribution to events from the teachers, and there is some documented evidence of opportunities to meet.~The meeting minutes indicate that meetings are directed by the Principal');// SCHOOL_RATING text
+date_default_timezone_set("Asia/Kolkata"); // Default timezone
+define('OFFLINE_STATUS', FALSE); // Status to manage application is using online or offline
+define("DIAGNOSTIC_LANG",'hi,en,ma'); // Diagnostic can be created in these languages (hi=hindi,en=english,ma=marathi)
+define("DEFAULT_LANGUAGE",'9'); // Default lanuage is 9 (9=English)
+define("DOWNLOAD_CHART_URL","https://www.highcharts.com/"); // Please set your Highchart url
 
